0c70f0fadf74962d79b93fa321c463d898f0f167
danyGhassan <dany.ghassa@gmail.com>
2024-02-14 09:36:36 +0100

    test

--- idk_mysite/IDK/models.py
+++ idk_mysite/IDK/models.py
@@ -14,3 +14,4 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text    
+ 
--- idk_mysite/IDK/views.py
+++ idk_mysite/IDK/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>ma grosse beteuh</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "IDK/list.html" , {"ls":ls})
 
-def v1(response):
-    return HttpResponse("<h1>view 1!</h1>")
+def home(response):
+    return render(response, "IDK/home.html" , {})
+
+def create(response):
+    if response.method == "POST" :
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" %t.id)
+    else :        
+        form = CreateNewList()
+    return render(response, "IDK/create.html" , {"form":form})
